81fc703ad167a8a29e166c86e5d383137bd4f585
Amasi-120 <amasii120@icloud.com>
2026-07-29 19:01:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,18 @@
-from fastapi import FastAPI, UploadFile, File
+import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
-import io
 
-app = FastAPI()
-
-# تحميل نموذج YOLO
 model = YOLO("best.pt")
 
+st.title("Smart Shopping Cart")
 
-@app.get("/")
-def home():
-    return {"message": "Smart Shopping Cart API is running"}
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
+if uploaded_file:
+    image = Image.open(uploaded_file)
 
-@app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-
-    image_bytes = await file.read()
-    image = Image.open(io.BytesIO(image_bytes))
+    st.image(image)
 
     results = model(image)
 
-    detections = []
-
-    for result in results:
-        for box in result.boxes:
-            confidence = float(box.conf[0])
-
-            if confidence > 0.1:
-                class_id = int(box.cls[0])
-
-                detections.append({
-                    "class_id": class_id,
-                    "confidence": confidence,
-                    "product": model.names[class_id]
-                })
-
-    return {
-        "detections": detections
-    }
+    st.write(results)
